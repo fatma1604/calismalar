a56cca1bceab3eaf47b7001b29f7b6ff8fd178c7
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2023-11-10 16:35:46 +0300

    dosya değişikliği
    değsşiklik

--- emuletorcalismalari/lib/butonCalısması.dart
+++ emuletorcalismalari/lib/butonCalısması.dart
@@ -121,30 +121,3 @@ class Buton extends StatelessWidget {
 }
 
 
-
-class YaziDegsikleri extends StatelessWidget {
-  //1-maloer görumesi için Scaffold eklicez
-  //2- sitil vericez texsin içine vericez style için de istedini yapa bilirsin
-  //ben  asadaki yazıyı büyütmrk istiyorum font size kulanıyorum
-// ben asadaki yazıyı kalın yapmak istedim   fontWeight: FontWeight.bold kulandım
-  //3- ortalıcaz
-  const YaziDegsikleri({super.key});
-  Widget build(BuildContext buildContext) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Fatma Nur Kamıs",
-          style: TextStyle(
-              fontSize: 45,
-              color: Color.fromARGB(255, 233, 6, 82),
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-    ); // scaffold kulanımı
-  }
-}
-/*
-yukardaki koda ilk scaffoldu ekledik sonra body ->sonra  ortaladdık ortalamayı şu şekilde yapıyoruz= //Yazıyı ortalamak için Texti mouse
- ile seçtik ve daha sonra Refactor seçip Center yaptım.ctrl+sift+r=kısa yol.
- sonra texte styla verdik bu şekilde:)
-*/
