4e41bc522d49d8cf43f2cca6e0ebca19095f5626
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2023-12-11 18:55:37 +0300

    sayfaya geri dönme
    geri dönme

---
+++ udmyileCalisma/navasin/isimlendirilmisrota/android/app/src/main/kotlin/com/example/isimlendirilmisrota/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.isimlendirilmisrota
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- udmyileCalisma/navasin/isimlendirilmisrota/test/widget_test.dart
+++ udmyileCalisma/navasin/isimlendirilmisrota/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bironcekisayfayagitme/main.dart';
+import 'package:isimlendirilmisrota/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
