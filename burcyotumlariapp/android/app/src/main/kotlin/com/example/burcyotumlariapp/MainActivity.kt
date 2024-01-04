c85dc4695927cdb8e6aa044dd0cd8f5036c549d8
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2024-01-04 19:24:30 +0300

    dosya silmne
    dosya silme

--- burcyotumlariapp/android/app/src/main/kotlin/com/example/burcyotumlariapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.burcyotumlariapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- emuletorcalismalari/lib/butonCalısması.dart
+++ emuletorcalismalari/lib/butonCalısması.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(home: YaziGetirme()));
 }
 //yun yapma kısmına geldik onlar ödev2 de
-
 
 //ben butona bastımda adam bana bi ekran çıkartsın veya bi yazı yasın isterim
 class YaziGetirme extends StatefulWidget {
@@ -29,78 +27,59 @@ class _YaziGetirme extends State<YaziGetirme> {
     return Scaffold(
       body: Center(
         child: SizedBox(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                text,
-                style: TextStyle(
-                    fontSize: 45,
-                    color: Color.fromARGB(255, 213, 96, 43),
-                    fontWeight: FontWeight.bold),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  yaziyiDegistir();
-                },
-                child: const Text("oyuna Başla"),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 215, 243, 33),
-                     
-              )
-            ],
-          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              text,
+              style: TextStyle(
+                  fontSize: 45,
+                  color: Color.fromARGB(255, 213, 96, 43),
+                  fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                yaziyiDegistir();
+              },
+              child: Text("oyuna basla "),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            ),
+          ]),
         ),
       ),
     );
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //butonda bişeyleri değtirmek istiyoSam ne kulanıcam
 //-->StatefulWidge
 
 class Buton2 extends StatefulWidget {
   const Buton2({super.key});
-  
+
   State<Buton2> createState() {
     return _Buton2();
   }
 }
 
-class _Buton2 extends State<Buton2>{ 
-
-Widget build(BuildContext buildContext) {
-  return Scaffold(
-    body: Center(child: Column(
-       mainAxisAlignment:MainAxisAlignment.center,
-      children: [
-        Text("buton"),
-         ElevatedButton(
+class _Buton2 extends State<Buton2> {
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("buton"),
+          ElevatedButton(
             onPressed: () {},
             child: const Text("butonn"),
           ),
-      ],
-    )),
-  );
+        ],
+      )),
+    );
+  }
 }
-}
-
 
 //buton eklemek
-//butonlarada sitil vere biliriz style: ElevatedButton.styleFrom 
+//butonlarada sitil vere biliriz style: ElevatedButton.styleFrom
 class Buton extends StatelessWidget {
   const Buton({super.key});
   Widget build(BuildContext buildContext) {
@@ -119,5 +98,3 @@ class Buton extends StatelessWidget {
     );
   }
 }
-
-
