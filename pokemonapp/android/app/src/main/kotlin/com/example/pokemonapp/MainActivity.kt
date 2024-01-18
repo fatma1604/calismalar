76e10e41f041733d7e32f7019ca6e8cbe0d208c8
Fatma Nur Kamış <120199233+fatma1604@users.noreply.github.com>
2024-01-18 18:31:13 +0300

    Delete pokemonapp directory

--- pokemonapp/android/app/src/main/kotlin/com/example/pokemonapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokemonapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- pokemonapp/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:pokemonapp/pages/hoem_page.dart';
-
-void main() {
-  runApp(const MaterialApp(home: HoemPage()));
-}
